10f593956386801c4a262d9d142dd359fd83f8a8
DaveFullStack <119569407+DaveFullStack@users.noreply.github.com>
2023-07-11 11:48:15 +0100

    created basic script fr player movement using the InputSystem in unity

--- Player/Movement/PlayerController.cs
+++ Player/Movement/PlayerController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed = 3f;
+    // Using public float so user can change in the inspector to test different moveSpeeds etc.
+
+    private Rigidbody2D _rb;
+    private Vector2 moveDirection;
+
+
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    private void FixedUpdate()
     {
-        
+        _rb.position = (moveDirection * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        moveDirection = value.Get<Vector2>().normalized;
     }
 }
